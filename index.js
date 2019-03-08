590fee4cca931185d05d2b2c599e539263cbb81b
Israel Michael da Silva Figueiredo <israel.figueiredo@mq.local>
2019-03-08 17:54:07 -0300

    added new output text method

--- index.js
+++ index.js
@@ -4,6 +4,15 @@ const stdoutput = (out) => { process.stdout.write(out), reset(); return output; 
 const reset = () => { process.stdout.write(colors.Reset); };
 
 const output = {
+  
+  out (paramout = '', textcolor = '', bgcolor = '') {
+    return stdoutput(prepareString(colors[`Fg${textcolor}`], colors[`Bg${bgcolor}`], paramout));
+  },
+  outln (paramout = '', textcolor = '', bgcolor = '') {
+    return stdoutput(prepareString(colors[`Fg${textcolor}`], colors[`Bg${bgcolor}`], paramout + '\r\n'));
+  },
+
+  //output text with colors
   black: (paramout, bgcolor) => stdoutput(prepareString(colors.FgBlack, colors[`Bg${bgcolor}`], paramout)),
   red: (paramout, bgcolor) => stdoutput(prepareString(colors.FgRed, colors[`Bg${bgcolor}`], paramout)),
   green: (paramout, bgcolor) => stdoutput(prepareString(colors.FgGreen, colors[`Bg${bgcolor}`], paramout)),
@@ -12,6 +21,8 @@ const output = {
   magenta: (paramout, bgcolor) => stdoutput(prepareString(colors.FgMagenta, colors[`Bg${bgcolor}`], paramout)),
   cyan: (paramout, bgcolor) => stdoutput(prepareString(colors.FgCyan, colors[`Bg${bgcolor}`], paramout)),
   white: (paramout, bgcolor) => stdoutput(prepareString(colors.FgWhite, colors[`Bg${bgcolor}`], paramout)),
+
+  //output text with background colors
   bgblack: (paramout, textcolor) => stdoutput(prepareString(colors.BgBlack, colors[`Fg${textcolor}`], paramout)),
   bgred: (paramout, textcolor) => stdoutput(prepareString(colors.BgRed, colors[`Fg${textcolor}`], paramout)),
   bggreen: (paramout, textcolor) => stdoutput(prepareString(colors.BgGreen, colors[`Fg${textcolor}`], paramout)),
@@ -21,7 +32,9 @@ const output = {
   bgcyan: (paramout, textcolor) => stdoutput(prepareString(colors.BgCyan, colors[`Fg${textcolor}`], paramout)),
   bgwhite: (paramout, textcolor) => stdoutput(prepareString(colors.BgWhite, colors[`Fg${textcolor}`], paramout)),
   
+  //output space
   spc: (numberspace = 1) => stdoutput(' '.repeat(numberspace)),
+  //output new line
   ln: (numberln = 1) => stdoutput('\r\n'.repeat(numberln)),
 };
 
--- tests/out.js
+++ tests/out.js
@@ -6,4 +6,8 @@ output.white('npm')
   .spc()
   .white('created a lockfile as package-lock.json. You should commit this file')
   .ln();
-output.white('npm').spc().bgyellow('WARN', 'Black').spc().white('output@1.0.0 No repository field.');
+output.white('npm').spc().bgyellow('WARN', 'Black').spc().white('output@1.0.0 No repository field.').ln();
+
+output
+  .ln()
+  .outln('Simple output', 'Cyan', 'White');
