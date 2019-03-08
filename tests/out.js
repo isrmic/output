b01970dae5ab60c8eb2bf371aa628cf9b931387b
Israel Michael da Silva Figueiredo <israel.figueiredo@mq.local>
2019-03-08 17:31:29 -0300

    modified tests / out.js

--- tests/out.js
+++ tests/out.js
@@ -1,4 +1,9 @@
 const { output } = require('../index.js');
 
-output.yellow('Pending: ').spc().green('OK').ln();
-output.bgblue(' INSTALAÇÂO ', 'Black');
+output.white('npm')
+  .spc()
+  .blue('notice')
+  .spc()
+  .white('created a lockfile as package-lock.json. You should commit this file')
+  .ln();
+output.white('npm').spc().bgyellow('WARN', 'Black').spc().white('output@1.0.0 No repository field.');
